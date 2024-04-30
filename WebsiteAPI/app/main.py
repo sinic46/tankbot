a528927425c9cdd944c7aaa250a464777991b60f
Simon Nicholson <sdnicholson09@gmail.com>
2024-04-30 23:17:33 +0100

    adding in logging for the api

--- WebsiteAPI/app/main.py
+++ WebsiteAPI/app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
+import logging
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    logger.info(f"request / endpoint!")
     return{"message":"The tank will see you now."}
 
 
 
 @app.post("/direction/{x}/{y}")
 def movement_direction(x:int,y:int):
+    logger.info(f"requested[/direction/{x}/{y}]")
+
     return{"message":f"the x axis is {x} and the y axis is {y}"}
 
 

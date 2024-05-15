1db89f53eeb3c50c7d5fa790fdfeac4d27b008a4
Simon Nicholson <sdnicholson09@gmail.com>
2024-05-15 13:27:21 +0100

    start on tank python code

---
+++ PiTank/main.py
@@ -0,0 +1,15 @@
+from requests import get
+
+
+def light_the_beacon():
+
+    response = get("https://robotapi.sinic.co.uk")
+
+    print(f"beacons are lit. {response.text}")
+
+
+
+
+
+if __name__ == "__main__":
+    light_the_beacon()
